eb15bb2706bfde852f71bba60cf4c99bda896809
Aurelien Floutard <aurelien.floutard@openindus.com>
2023-12-29 16:19:00 +0100

    testing RS User and updatinng RS485 receiver example

--- examples/RS485_Receiver.cpp
+++ examples/RS485_Receiver.cpp
@@ -4,18 +4,11 @@
 
 void setup()
 {
-    RS485.begin();
+    
 }
 
 void loop()
 {
-    int data;
-
-    if (RS485.available())
-    {
-        data = RS485.read();
-        printf("%d\n", data);
-    }
-    delay(10);
+    delay(1000);
 }
 
--- main/main.cpp
+++ main/main.cpp
@@ -1,12 +1,21 @@
 #include "OpenIndus.h"
 #include "Arduino.h"
+#include "RS.h"
 
 void setup()
 {
-
+    RS485.begin();
 }
 
 void loop()
 {
+    int data;
 
+    if (RS485.available())
+    {
+        data = RS485.read();
+        printf("%c\n", data);
+    }
+    delay(10);
 }
+
