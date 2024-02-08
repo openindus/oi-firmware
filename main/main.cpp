e3abf9e83a99d8f36ec8819d9411d9bd9206d61c
Aurelien Floutard <aurelien.floutard@openindus.com>
2024-02-08 11:41:57 +0100

    clean main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -1,10 +1,6 @@
 #include "OpenIndus.h"
 #include "Arduino.h"
 
-Stepper s1;
-Stepper s2;
-Stepper s3;
-
 void setup(void)
 {
     printf("Hello World !\n");
@@ -12,12 +8,5 @@ void setup(void)
 
 void loop(void)
 {
-    s1.ledOn(LED_PURPLE);
-    s2.ledOn(LED_CYAN);
-    s3.ledOn(LED_YELLOW);
-    delay(1000);
-    s1.ledOff();
-    s2.ledOff();
-    s3.ledOff();
     delay(1000);
 }
