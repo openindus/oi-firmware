8b4c931c126e561fed18cc653b7f3783d6ae9e89
Kévin Lefeuvre <kevin.lefeuvre@openindus.com>
2024-03-08 14:09:38 +0100

    feat(ADC): Add ADS114S0x ADC driver

---
+++ components/drivers/adc/ads114s0x/ads114s0x.c
@@ -0,0 +1,9 @@
+/**
+ * @file ads114s0x.c
+ * @brief ADS114S0x ADC driver
+ * @author Kevin Lefeuvre (kevin.lefeuvre@openindus.com)
+ * @copyright (c) [2024] OpenIndus, Inc. All rights reserved.
+ * @see https://openindus.com
+ */
+
+#include "ads114s0x.h"
---
+++ components/drivers/adc/ads114s0x/ads114s0x.h
@@ -0,0 +1,25 @@
+/**
+ * @file ads114s0x.h
+ * @brief ADS114S0x ADC driver
+ * @author Kevin Lefeuvre (kevin.lefeuvre@openindus.com)
+ * @copyright (c) [2024] OpenIndus, Inc. All rights reserved.
+ * @see https://openindus.com
+ */
+
+#pragma once
+
+#include "esp_err.h"
+#include "esp_log.h"
+#include "driver/gpio.h"
+#include "driver/spi_master.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
