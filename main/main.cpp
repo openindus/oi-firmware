afc39b0a4a27f6032800b110c12182e9ea654ce0
Aurelien Floutard <aurelien.floutard@openindus.com>
2024-04-25 18:23:48 +0200

    calibration ok

--- main/main.cpp
+++ main/main.cpp
@@ -5,15 +5,10 @@ OICore core;
 
 void setup(void)
 {
-    // Serial.begin(115200);
-    // Serial.println("Hello World !");
-    // float a[2] = {11.797, 11.797};
-    // float b[2] = {278.94, 278.94};
 
-    // core.setAnalogCoeffs(a, b);
 }
 
 void loop(void)
 {
-    delay(1000);
+    delay(100);
 }
