c2871d2abee5f2287de7fb056e2c1748af114d8d
Aurelien Floutard <aurelien.floutard@openindus.com>
2024-04-30 16:38:27 +0200

    update library.json and clean platformio.ini and main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -1,18 +1,13 @@
 #include "OpenIndus.h"
 #include "Arduino.h"
 
-OIDiscrete d;
-
 void setup(void)
 {
-    d.digitalModePWM(DOUT_1, 100);
+    Serial.begin(115200);
+    Serial.println("Hello World !");
 }
 
 void loop(void)
 {
-    d.digitalSetPWM(DOUT_1, (uint32_t)(16383.0*0.5)/10); // 1ms
-    delay(1000);
-    
-    d.digitalSetPWM(DOUT_1, (uint32_t)(16383.0*2)/10); // 2ms
     delay(1000);
 }
