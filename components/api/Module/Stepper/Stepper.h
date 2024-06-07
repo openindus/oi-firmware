adca2575a63c624caddc7086c65a4a888c5455e1
Kévin Lefeuvre <kevin.lefeuvre@openindus.com>
2024-06-07 09:35:22 +0200

    Clean stepper

--- components/api/Module/Stepper/Stepper.h
+++ components/api/Module/Stepper/Stepper.h
@@ -1,15 +1,8 @@
 /**
- * Copyright (C) OpenIndus, Inc - All Rights Reserved
- *
- * This file is part of OpenIndus Library.
- *
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- * 
  * @file Stepper.h
- * @brief Functions for Stepper module
- *
- * For more information on OpenIndus:
+ * @brief Stepper
+ * @author
+ * @copyright (c) [2024] OpenIndus, Inc. All rights reserved.
  * @see https://openindus.com
  */
 
@@ -18,48 +11,61 @@
 #include "Global.h"
 #include "Module.h"
 
-#if defined(OI_STEPPER) || defined(OI_STEPPER_VE)
-#include "StepperPinout.h"
-#include "StepperParam.h"
-#include "DigitalInputs.h"
-#include "MotorStepper.h"
-#include "CLI_DIn.h"
-#include "CLI_Stepper.h"
-#include "CLI_StepperParam.h"
-#endif
-
 #if (defined(OI_STEPPER) || defined(OI_STEPPER_VE)) && defined(MODULE_STANDALONE)
 #define Stepper StepperStandalone
 #elif (defined(OI_STEPPER) || defined(OI_STEPPER_VE)) && defined(MODULE_SLAVE)
 #define Stepper StepperSlave
-#include "BusCtrlSlave_DIn.h"
-#include "BusCtrlSlave_Stepper.h"
 #elif defined(MODULE_MASTER) 
-#define Stepper StepperBusCtrl
-#include "BusCtrlCmd_DIn.h"
-#include "BusCtrlCmd_Stepper.h"
+#define Stepper StepperCmd
 #endif
 
 #if defined(OI_STEPPER) || defined(OI_STEPPER_VE)
 
-class StepperCLI : public CLI_DIn, public CLI_Stepper, public MotorStepperParam
+#include "StepperPinout.h"
+#include "StepperParam.h"
+#include "Module.h"
+#include "DigitalInputs.h"
+#include "MotorStepper.h"
+#include "MotorStepperParam.h"
+
+class StepperStandalone : 
+    public ModuleStandalone, 
+    public DigitalInputs, 
+    public MotorStepper,
+    public MotorStepperParam
 {
 public:
 
     static int init(void);
 };
 
-class StepperStandalone : public ModuleStandalone, public DigitalInputs, public MotorStepper
+#include "CLI_DIn.h"
+#include "CLI_Stepper.h"
+#include "CLI_StepperParam.h"
+
+class StepperCLI : 
+    public CLI_DIn, 
+    public CLI_Stepper,
+    public CLI_StepperParam
 {
 public:
 
     static int init(void);
 };
+
+
 #endif
 
 #if (defined(OI_STEPPER) || defined(OI_STEPPER_VE)) && defined(MODULE_SLAVE)
 
-class StepperSlave : public BusCtrlSlave, public StepperStandalone,  public BusCtrlSlave_DIn, public BusCtrlSlave_Stepper
+#include "BusCtrlSlave.h"
+#include "BusCtrlSlave_DIn.h"
+#include "BusCtrlSlave_Stepper.h"
+
+class StepperSlave : 
+    public BusCtrlSlave,
+    public BusCtrlSlave_DIn, 
+    public BusCtrlSlave_Stepper
 {
 public:
 
@@ -68,11 +74,18 @@ public:
 
 #elif defined(MODULE_MASTER)
 
-class StepperBusCtrl : public BusCtrlCmd, public BusCtrlCmd_DIn, public BusCtrlCmd_Stepper
+#include "BusCtrlCmd.h"
+#include "BusCtrlCmd_DIn.h"
+#include "BusCtrlCmd_Stepper.h"
+
+class StepperCmd : 
+    public BusCtrlCmd, 
+    public BusCtrlCmd_DIn, 
+    public BusCtrlCmd_Stepper
 {
 public:
 
-    StepperBusCtrl(uint32_t sn = 0) : 
+    StepperCmd(uint32_t sn = 0) : 
         BusCtrlCmd(TYPE_OI_STEPPER, sn),
         BusCtrlCmd_DIn(this),
         BusCtrlCmd_Stepper(this) {}
