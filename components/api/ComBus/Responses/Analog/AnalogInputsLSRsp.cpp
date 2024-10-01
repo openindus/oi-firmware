496297ef666d466cbba497034501cf6e58e329f2
Aurelien Floutard <aurelien.floutard@openindus.com>
2024-10-01 14:26:18 +0200

    add analogls rsp

---
+++ components/api/ComBus/Responses/Analog/AnalogInputsLSRsp.cpp
@@ -0,0 +1,96 @@
+/**
+ * Copyright (C) OpenIndus, Inc - All Rights Reserved
+ *
+ * This file is part of OpenIndus Library.
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * 
+ * @file AnalogInputsLSRsp.h
+ *
+ * For more information on OpenIndus:
+ * @see https://openindus.com
+ */
+
+#include "AnalogInputsLSRsp.h"
+
+#if defined(MODULE_SLAVE)
+
+int AnalogInputsLSRsp::init(void)
+{
+    
+    ControllerSlave::addCtrlCallback(REQUEST_ADD_SENSOR, [](std::vector<uint8_t>& data) {
+        const std::vector<AIn_Num_t> aIns = std::vector<AIn_Num_t>(data.begin()+1, data.end());
+        int index = AnalogInputsLS::addSensor((Sensor_Type_e)data[1], aIns);
+        data.clear();
+        data.push_back(REQUEST_ADD_SENSOR);
+        data.push_back((uint8_t)index);
+    });
+
+    ControllerSlave::addCtrlCallback(REQUEST_SET_STABILIZATION_TIME, [](std::vector<uint8_t>& data) {
+        int* value = reinterpret_cast<int*>(&data[1]);
+        AnalogInputsLS::setStabilizationTime(*value);
+        data.clear();
+    });
+
+    ControllerSlave::addCtrlCallback(REQUEST_SET_ACQUISITION_TIME, [](std::vector<uint8_t>& data) {
+        AnalogInputsLS::setAcquisitionTime((AcquisitionDuration_e)data[1]);
+        data.clear();
+    });
+
+    ControllerSlave::addCtrlCallback(REQUEST_RAW_SENSOR_SET_GAIN, [](std::vector<uint8_t>& data) {
+        AnalogInputsLS::raw[data[1]].setGain((Sensor_Gain_e)data[2]);
+        data.clear();
+    });
+
+    ControllerSlave::addCtrlCallback(REQUEST_RAW_SENSOR_READ, [](std::vector<uint8_t>& data) {
+        int16_t value = AnalogInputsLS::raw[data[1]].read();
+        uint8_t* ptr = reinterpret_cast<uint8_t*>(&value);
+        data.insert(data.end(), ptr, ptr + sizeof(int16_t));
+    });
+
+    ControllerSlave::addCtrlCallback(REQUEST_RAW_SENSOR_READ_MILLIVOLT, [](std::vector<uint8_t>& data) {
+        float value = AnalogInputsLS::raw[data[1]].readMillivolts();
+        uint8_t* ptr = reinterpret_cast<uint8_t*>(&value);
+        data.insert(data.end(), ptr, ptr + sizeof(float));
+    });
+
+    ControllerSlave::addCtrlCallback(REQUEST_RTD_READ_RESISTOR, [](std::vector<uint8_t>& data) {
+        float value = AnalogInputsLS::rtd[data[1]].readResistor();
+        uint8_t* ptr = reinterpret_cast<uint8_t*>(&value);
+        data.insert(data.end(), ptr, ptr + sizeof(float));
+    });
+
+    ControllerSlave::addCtrlCallback(REQUEST_RTD_READ_TEMPERATURE, [](std::vector<uint8_t>& data) {
+        float value = AnalogInputsLS::rtd[data[1]].readTemperature();
+        uint8_t* ptr = reinterpret_cast<uint8_t*>(&value);
+        data.insert(data.end(), ptr, ptr + sizeof(float));
+    });
+
+    ControllerSlave::addCtrlCallback(REQUEST_TC_READ_MILLIVOLTS, [](std::vector<uint8_t>& data) {
+        float value = AnalogInputsLS::tc[data[1]].readMillivolts();
+        uint8_t* ptr = reinterpret_cast<uint8_t*>(&value);
+        data.insert(data.end(), ptr, ptr + sizeof(float));
+    });
+
+    ControllerSlave::addCtrlCallback(REQUEST_TC_READ_TEMPERATURE, [](std::vector<uint8_t>& data) {
+        float value = AnalogInputsLS::tc[data[1]].readTemperature();
+        uint8_t* ptr = reinterpret_cast<uint8_t*>(&value);
+        data.insert(data.end(), ptr, ptr + sizeof(float));
+    });
+
+    ControllerSlave::addCtrlCallback(REQUEST_SG_READ, [](std::vector<uint8_t>& data) {
+        float value = AnalogInputsLS::sg[data[1]].read();
+        uint8_t* ptr = reinterpret_cast<uint8_t*>(&value);
+        data.insert(data.end(), ptr, ptr + sizeof(float));
+    });
+
+    ControllerSlave::addCtrlCallback(REQUEST_SG_SET_EXCITATION_MODE, [](std::vector<uint8_t>& data) {
+        AnalogInputsLS::sg[data[1]].setExcitationMode((StrainGauge_Excitation_e)data[2]);
+        data.clear();
+    });
+
+    return 0;
+}
+
+#endif
--- main/main.cpp
+++ main/main.cpp
@@ -1,38 +1,13 @@
 #include "OpenIndus.h"
 #include "Arduino.h"
 
-AnalogLS analogls;
-
 void setup(void)
 {
-    printf("Hello OpenIndus !\n");
-    delay(1000);
-
-    analogls.addSensor(RAW_SENSOR,{AIN_C_P, AIN_C_N});
-    // analogls.addSensor(RTD_TWO_WIRE, {AIN_B_P, AIN_B_N});
-    // analogls.addSensor(RTD_TWO_WIRE, {AIN_C_P, AIN_C_N});
-    // analogls.addSensor(RTD_TWO_WIRE, {AIN_D_P, AIN_D_N});
-    // analogls.addSensor(RTD_TWO_WIRE, {AIN_E_P, AIN_E_N});
-    analogls.setStabilizationTime(1000);
-    analogls.setAcquisitionTime(SAMPLE_50_MS);
-    // analogls.raw[0].setGain(GAIN_4);
+    Serial.begin(115200);
+    Serial.println("Hello World !");
 }
 
 void loop(void)
 {
-    float data = 0.0;
-
-    /* RTD */
-    data = analogls.raw[0].readMillivolts();
-    printf(">RTD (CH0): %f\n", data);
-    // data = analogls.rtd[1].readTemperature();
-    // printf(">RTD (CH1) - Temperature(°C): %f\n", data);
-    // data = analogls.rtd[2].readTemperature();
-    // printf(">RTD (CH2) - Temperature(°C): %f\n", data);
-    // data = analogls.rtd[3].readTemperature();
-    // printf(">RTD (CH3) - Temperature(°C): %f\n", data);
-    // data = analogls.rtd[4].readTemperature();
-    // printf(">RTD (CH4) - Temperature(°C): %f\n", data);
-    printf("\n");
-    // delay(1000);
+    delay(1000);
 }
