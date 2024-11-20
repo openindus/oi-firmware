ad79479bfc4ac7eaa761e88841a3160dbe146ba5
Georges de Massol <georges.massol@openindus.com>
2024-11-20 17:05:40 +0100

    fix: RS not accepting config given by ModbusRTUClient.begin()

--- components/api/Middleware/RS/RS.cpp
+++ components/api/Middleware/RS/RS.cpp
@@ -15,7 +15,7 @@
  * @param mode RS_485 or RS_232
  * @param baudrate in bits per second (baud)
  */
-void RS::begin(RS_Mode_t mode, unsigned long baudrate)
+void RS::begin(RS_Mode_t mode, unsigned long baudrate, uint16_t config)
 {
     _mode = mode;
     
@@ -28,11 +28,15 @@ void RS::begin(RS_Mode_t mode, unsigned long baudrate)
     SC16IS750_create(&device_config); 
 
     SC16IS750_SerialParams_t conf;
+    
     conf.baudrate = baudrate;
     conf.flow_type = Disabled;
-    conf.format_bits = 8;
-    conf.format_stop_bits = 1;
-    conf.parity = None;
+    conf.format_bits = (((config >> 2) & 0x03)+5);
+    // int test = (((0x8000012 >> 2) & 0x03)+5);
+    conf.format_stop_bits = (((config >> 5) & 0x01)+1); 
+    //int test = ((0x800003f >> 5) & 0x01)+1; 
+    uint8_t parity = (config & 0x03);
+    conf.parity = ((parity == 0x00) ? None : (parity == 0x11) ? Odd : Even);
     conf.trigger_halt = TCR_HALT_DEFAULT;
     conf.trigger_resume = TCR_RESUME_DEFAULT;
 
