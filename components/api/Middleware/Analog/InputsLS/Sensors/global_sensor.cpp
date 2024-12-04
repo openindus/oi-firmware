a112dc4baf02e7bd588295e821280b1c9c1281c2
Mani Gillier <mani.gillier@openindus.com>
2024-12-04 15:28:46 +0100

    feat: added the global sensor file that adds the Sensor class
    This class is the mother class of all sensors and it will be the one
    stored with virtual functions to call different child functions.
    
    For example if you create a RawSensor, it will be stored as a Sensor
    and accessing it's raw_read method will be through the virtual
    raw_read method that will be overwritten by the RawSensor class.
    If a function is irrelevant in the mother class, or other classes, it
    will be defaulted by an error message.

---
+++ components/api/Middleware/Analog/InputsLS/Sensors/global_sensor.cpp
@@ -0,0 +1,106 @@
+/**
+ * @file global_sensor.cpp
+ * @brief Global sensor
+ * @author Mani Gillier (mani.gillier@openindus.com)
+ * @copyright (c) [2024] OpenIndus, Inc. All rights reserved.
+ * @see https://openindus.com
+ */
+
+#include "global_sensor.hpp"
+
+static const char TAG[] = "Sensor";
+
+void Sensor::route(void)
+{
+    AIn_Num_t hs_ain = _ainPins[_mux_config.hs_index];
+    AIn_Num_t ls_ain = _ainPins[_mux_config.ls_index];
+
+    /* Positive AIn is congruent with 0 modulo 2 */
+    if (hs_ain % 2 != 0) {
+        ESP_LOGE(TAG, "The sensor AIn (%d) set at index %d is not a positive AIn. No MUX ROUTE may occur.\n", hs_ain, _mux_config.hs_index);
+        return;
+    }
+    /* Positive AIn is congruent with 1 modulo 2 */
+    if (ls_ain % 2 != 1) {
+        ESP_LOGE(TAG, "The sensor AIn (%d) set at index %d is not a negative AIn. No MUX ROUTE may occur.\n", ls_ain, _mux_config.ls_index);
+        return;
+    }
+    /* Divided by 2 because only half of the AIns are in each MUX */
+    _highSideMux->route(_mux_config.input, hs_ain / 2);
+    _lowSideMux->route(ls_ain / 2, _mux_config.output);
+}
+
+/**
+ * @brief Initialise the read parameters and connections
+ */
+void Sensor::init_read(void)
+{
+    /* Route multiplexer */
+    route();
+
+    /* Set bias on negative input if activated */
+    if (_bias_active) {
+        _adc->setBias(static_cast<ads114s0x_adc_input_e>(_adcPins[1]));
+    } else {
+        _adc->setBias(ADS114S0X_NOT_CONNECTED);
+    }
+
+    /* Set Internal reference to selected ref */
+    _adc->setReference((ads114s0x_ref_selection_e)_reference);
+
+    /* Set Excitation mode to selected mode */
+    _adc->setExcitation((ads114s0x_idac_magnitude_e)_excitation);
+
+    /* Set PGA Gain */
+    _adc->setPGAGain((ads114s0x_pga_gain_e)_gain);
+
+    /* Wait for stabilization */
+    _adc->waitStabilization();
+}
+
+void Sensor::reset_read(void)
+{
+    /* Reset multiplexer route */
+    _highSideMux->route(INPUT_OPEN_HS, 0);
+    _lowSideMux->route(0, OUTPUT_OPEN_LS);
+
+    /* Reset bias */
+    _adc->setBias(ADS114S0X_NOT_CONNECTED);
+
+    /* Reset ref */
+    _adc->setReference(ADS114S0X_REF_INTERNAL_2_5V);
+
+    /* Reset excitation */
+    _adc->setExcitation(ADS114S0X_IDAC_OFF);
+
+    /* Reset gain */
+    _adc->setPGAGain(ADS114S0X_PGA_GAIN_1);
+
+    /* Reset Internal MUX */
+    _adc->setInternalMux(ADS114S0X_NOT_CONNECTED, ADS114S0X_NOT_CONNECTED);
+}
+
+/**
+ * @brief Read raw value
+ * 
+ * @return int16_t adcCode
+ */
+int16_t Sensor::raw_read(uint8_t positive_index, uint8_t negative_index)
+{
+    if (_adc == NULL) {
+        ESP_LOGE(TAG, "%s() error", __func__);
+        return 0;
+    }
+
+    init_read();
+
+    /* Set Internal mux */
+    _adc->setInternalMux(static_cast<ads114s0x_adc_input_e>(_adcPins[positive_index]), static_cast<ads114s0x_adc_input_e>(_adcPins[negative_index]));
+
+    /* ADC Read */
+    int16_t adcCode = _adc->read();
+
+    reset_read();
+
+    return adcCode;
+}
---
+++ components/api/Middleware/Analog/InputsLS/Sensors/global_sensor.hpp
@@ -0,0 +1,90 @@
+/**
+ * @file global_sensor.h
+ * @brief Global sensor
+ * @author Mani Gillier (mani.gillier@openindus.com)
+ * @copyright (c) [2024] OpenIndus, Inc. All rights reserved.
+ * @see https://openindus.com
+ */
+
+#pragma once
+
+#include "_ADS114S0X.h"
+#include "Multiplexer.h"
+#include "Sensor.h"
+
+struct Dual_Mux_Config_s {
+    Multiplexer_Input_e input;
+    Multiplexer_Output_e output;
+    uint8_t hs_index;
+    uint8_t ls_index;
+};
+
+#define SENSOR_TAG "Sensor"
+#define SENSOR_FUNCTIONNALITY_NOT_FOUND_MESSAGE "The sensor you requested doesn't have this functionnality (%s)."
+#define SENSOR_FUNCTIONNALITY_NOT_FOUND ESP_LOGE(SENSOR_TAG, SENSOR_FUNCTIONNALITY_NOT_FOUND_MESSAGE, __PRETTY_FUNCTION__);
+#define SENSOR_FUNCTIONNALITY_NOT_FOUND_RETURN(value) ESP_LOGE(SENSOR_TAG, SENSOR_FUNCTIONNALITY_NOT_FOUND_MESSAGE, __PRETTY_FUNCTION__); return value;
+
+class Sensor
+{
+    public:
+    Sensor(ADS114S0X* adc, Multiplexer* highSideMux, Multiplexer* lowSideMux, const Sensor_Pinout_s& pins, Sensor_Type_e type, uint32_t index) :
+        _adc(adc),
+        _highSideMux(highSideMux),
+        _lowSideMux(lowSideMux),
+        _ainPins(pins.ainPins),
+        _adcPins(pins.adcPins),
+        _gain(GAIN_1),
+        _reference(REFERENCE_INTERNAL_2_5V),
+        _bias_active(true),
+        _excitation(EXCITATION_OFF),
+        _mux_config({
+            .input = INPUT_OPEN_HS,
+            .output = OUTPUT_OPEN_LS,
+            .hs_index = 0,
+            .ls_index = 1,
+        }),
+        _type(type),
+        _index(index) {}
+
+    /* Found on all sensors */
+    virtual inline float read(void) { return (float) raw_read(); };
+    virtual int16_t raw_read(uint8_t positive_index = 0, uint8_t negative_index = 1);
+
+    /* Sets a parameter */
+    virtual inline void setParameter(Sensor_Parameter_e parameter, Sensor_Parameter_Value_u value) { SENSOR_FUNCTIONNALITY_NOT_FOUND };
+    /* Read the value converted in millivolts */
+    virtual inline float readMillivolts(void) { SENSOR_FUNCTIONNALITY_NOT_FOUND_RETURN(0.0) };
+    /* Read the resistance of an RTD */
+    virtual inline float readResistor(void) { SENSOR_FUNCTIONNALITY_NOT_FOUND_RETURN(0.0) };
+    /* Read the temperature */
+    virtual inline float readTemperature(void) { SENSOR_FUNCTIONNALITY_NOT_FOUND_RETURN(0.0) };
+    /* Set the excitation mode for the strain gauge */
+    virtual inline void setSGExcitationMode(StrainGauge_Excitation_e excitation) { SENSOR_FUNCTIONNALITY_NOT_FOUND };
+
+    protected:
+    /* Found on all sensors */
+    /* Used for the read method */
+    virtual void route(void);
+    virtual void init_read(void);
+    virtual void reset_read(void);
+    /* Used for the setParameter method */
+    virtual inline void setGain(Sensor_Gain_e gain) { SENSOR_FUNCTIONNALITY_NOT_FOUND };
+    virtual inline void setReference(Sensor_Ref_e reference) { SENSOR_FUNCTIONNALITY_NOT_FOUND };
+    virtual inline void setBiasActive(bool active) { SENSOR_FUNCTIONNALITY_NOT_FOUND };
+    virtual inline void setExcitation(Sensor_Excitation_e excitation) { SENSOR_FUNCTIONNALITY_NOT_FOUND };
+
+    ADS114S0X* _adc;
+    Multiplexer* _highSideMux;
+    Multiplexer* _lowSideMux;
+    std::array<AIn_Num_t, 4> _ainPins;
+    std::array<ADC_Input_t, 4> _adcPins;
+
+    Sensor_Gain_e _gain;
+    Sensor_Ref_e _reference;
+    bool _bias_active;
+    Sensor_Excitation_e _excitation;
+    Dual_Mux_Config_s _mux_config;
+
+    enum Sensor_Type_e _type;
+    uint32_t _index;
+};
