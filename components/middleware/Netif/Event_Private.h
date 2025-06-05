7da1cf809db3681498822d41b56bd2639210b0b4
Kévin Lefeuvre <kevin.lefeuvre@openindus.com>
2025-06-05 12:50:01 +0200

    Remove Netif and Cloud components

--- components/middleware/Netif/Event_Private.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Netif_Private.h"
-
-#define Task_StackDepth CONFIG_NETIF_TASK_STACK_DEPTH
-#define Task_Priority CONFIG_NETIF_TASK_PRIORITY
-
-void Netif_task_Connected(Netif_t *netif);
-void Netif_task_Disconnected(Netif_t *netif);
