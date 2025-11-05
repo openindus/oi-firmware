48b1c540e0cc4ddb0f19b95679e237c97e57ea99
Georges de Massol <georges.massol@openindus.com>
2025-11-05 17:08:08 +0100

    feat : Compilable code, but pinout is wrong on board. Please follow https://gitlab.openindus.com/openindus/oi-hard/oi-products/E2-MODU007/-/issues/8

--- components/openindus/api/Module/Discrete/DiscretePinout.h
+++ components/openindus/api/Module/Discrete/DiscretePinout.h
@@ -7,7 +7,7 @@
  */
 
 #pragma once
-
+#if defined(CONFIG_OI_DISCRETE) || defined(CONFIG_OI_DISCRETE_VE)
 #if defined(CONFIG_OI_DISCRETE)
 
 /* DIN */
@@ -31,6 +31,33 @@
 #define DISCRETE_GPIO_DOUT_6            GPIO_NUM_17
 #define DISCRETE_GPIO_DOUT_7            GPIO_NUM_18
 #define DISCRETE_GPIO_DOUT_8            GPIO_NUM_19
+
+#elif defined(CONFIG_OI_DISCRETE_VE)
+
+/* DIN */
+#define DISCRETE_GPIO_DIN_1             GPIO_NUM_20
+#define DISCRETE_GPIO_DIN_2             GPIO_NUM_21
+#define DISCRETE_GPIO_DIN_3             GPIO_NUM_21 //GPIO_NUM_22 // Different GPIO pin compared to CONFIG_OI_DISCRETE and does not even exist !!!
+#define DISCRETE_GPIO_DIN_4             GPIO_NUM_31 // Different GPIO pin compared to CONFIG_OI_DISCRETE
+#define DISCRETE_GPIO_DIN_5             GPIO_NUM_32 // Different GPIO pin compared to CONFIG_OI_DISCRETE
+#define DISCRETE_GPIO_DIN_6             GPIO_NUM_33
+#define DISCRETE_GPIO_DIN_7             GPIO_NUM_34
+#define DISCRETE_GPIO_DIN_8             GPIO_NUM_35
+#define DISCRETE_GPIO_DIN_9             GPIO_NUM_36
+#define DISCRETE_GPIO_DIN_10            GPIO_NUM_37
+
+/* DOUT */
+#define DISCRETE_GPIO_DOUT_1            GPIO_NUM_13
+#define DISCRETE_GPIO_DOUT_2            GPIO_NUM_10
+#define DISCRETE_GPIO_DOUT_3            GPIO_NUM_15
+#define DISCRETE_GPIO_DOUT_4            GPIO_NUM_14
+#define DISCRETE_GPIO_DOUT_5            GPIO_NUM_16
+#define DISCRETE_GPIO_DOUT_6            GPIO_NUM_17
+#define DISCRETE_GPIO_DOUT_7            GPIO_NUM_18
+#define DISCRETE_GPIO_DOUT_8            GPIO_NUM_19
+
+#endif
+
 #define DISCRETE_ADC_CHANNEL_DOUT_1     ADC_CHANNEL_0
 #define DISCRETE_ADC_CHANNEL_DOUT_2     ADC_CHANNEL_1
 #define DISCRETE_ADC_CHANNEL_DOUT_3     ADC_CHANNEL_3
@@ -45,46 +72,5 @@
 #define DISCRETE_ADC_CHANNEL_AIN_1      ADC_CHANNEL_0
 #define DISCRETE_ADC_CHANNEL_AIN_2      ADC_CHANNEL_1
 #define DISCRETE_ADC_UNIT_AIN           ADC_UNIT_2
-
-#elif defined(CONFIG_OI_DISCRETE_VE)
-
-/* TODO: Configure DIN GPIO pins for DISCRETE_VE */
-/* DIN */
-#define DISCRETE_GPIO_DIN_1             GPIO_NUM_XX  // TODO: Replace XX with actual GPIO number
-#define DISCRETE_GPIO_DIN_2             GPIO_NUM_XX  // TODO: Replace XX with actual GPIO number
-#define DISCRETE_GPIO_DIN_3             GPIO_NUM_XX  // TODO: Replace XX with actual GPIO number
-#define DISCRETE_GPIO_DIN_4             GPIO_NUM_XX  // TODO: Replace XX with actual GPIO number
-#define DISCRETE_GPIO_DIN_5             GPIO_NUM_XX  // TODO: Replace XX with actual GPIO number
-#define DISCRETE_GPIO_DIN_6             GPIO_NUM_XX  // TODO: Replace XX with actual GPIO number
-#define DISCRETE_GPIO_DIN_7             GPIO_NUM_XX  // TODO: Replace XX with actual GPIO number
-#define DISCRETE_GPIO_DIN_8             GPIO_NUM_XX  // TODO: Replace XX with actual GPIO number
-#define DISCRETE_GPIO_DIN_9             GPIO_NUM_XX  // TODO: Replace XX with actual GPIO number
-#define DISCRETE_GPIO_DIN_10            GPIO_NUM_XX  // TODO: Replace XX with actual GPIO number
-
-/* TODO: Configure DOUT GPIO pins and ADC channels for DISCRETE_VE */
-/* DOUT */
-#define DISCRETE_GPIO_DOUT_1            GPIO_NUM_XX  // TODO: Replace XX with actual GPIO number
-#define DISCRETE_GPIO_DOUT_2            GPIO_NUM_XX  // TODO: Replace XX with actual GPIO number
-#define DISCRETE_GPIO_DOUT_3            GPIO_NUM_XX  // TODO: Replace XX with actual GPIO number
-#define DISCRETE_GPIO_DOUT_4            GPIO_NUM_XX  // TODO: Replace XX with actual GPIO number
-#define DISCRETE_GPIO_DOUT_5            GPIO_NUM_XX  // TODO: Replace XX with actual GPIO number
-#define DISCRETE_GPIO_DOUT_6            GPIO_NUM_XX  // TODO: Replace XX with actual GPIO number
-#define DISCRETE_GPIO_DOUT_7            GPIO_NUM_XX  // TODO: Replace XX with actual GPIO number
-#define DISCRETE_GPIO_DOUT_8            GPIO_NUM_XX  // TODO: Replace XX with actual GPIO number
-#define DISCRETE_ADC_CHANNEL_DOUT_1     ADC_CHANNEL_X  // TODO: Replace X with actual ADC channel
-#define DISCRETE_ADC_CHANNEL_DOUT_2     ADC_CHANNEL_X  // TODO: Replace X with actual ADC channel
-#define DISCRETE_ADC_CHANNEL_DOUT_3     ADC_CHANNEL_X  // TODO: Replace X with actual ADC channel
-#define DISCRETE_ADC_CHANNEL_DOUT_4     ADC_CHANNEL_X  // TODO: Replace X with actual ADC channel
-#define DISCRETE_ADC_CHANNEL_DOUT_5     ADC_CHANNEL_X  // TODO: Replace X with actual ADC channel
-#define DISCRETE_ADC_CHANNEL_DOUT_6     ADC_CHANNEL_X  // TODO: Replace X with actual ADC channel
-#define DISCRETE_ADC_CHANNEL_DOUT_7     ADC_CHANNEL_X  // TODO: Replace X with actual ADC channel
-#define DISCRETE_ADC_CHANNEL_DOUT_8     ADC_CHANNEL_X  // TODO: Replace X with actual ADC channel
-#define DISCRETE_ADC_UNIT_DOUT          ADC_UNIT_1  // TODO: Verify if ADC_UNIT_1 is correct for VE
-
-/* TODO: Configure AIN ADC channels for DISCRETE_VE */
-/* AIN */
-#define DISCRETE_ADC_CHANNEL_AIN_1      ADC_CHANNEL_X  // TODO: Replace X with actual ADC channel
-#define DISCRETE_ADC_CHANNEL_AIN_2      ADC_CHANNEL_X  // TODO: Replace X with actual ADC channel
-#define DISCRETE_ADC_UNIT_AIN           ADC_UNIT_2  // TODO: Verify if ADC_UNIT_2 is correct for VE
 
 #endif
