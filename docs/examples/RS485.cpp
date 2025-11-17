095cea8560bb6aa050367bf4c241aa1ae900d6f5
Kévin Lefeuvre <kevin.lefeuvre@openindus.com>
2025-11-17 16:18:53 +0100

    fix: RS example errors

--- docs/examples/RS485.cpp
+++ docs/examples/RS485.cpp
@@ -7,19 +7,22 @@ void setup(void)
 {
     Serial.begin(115200);
     
+    /* Initialize RS485 */
     core.rs.begin(9600, RS::Config::_8N1, RS::Mode::RS485);
 }
 
 void loop(void)
 {
-    char data[100];
-    size_t size = core.rs.available();
-
+    uint8_t data[64] = {0};
+    size_t size = 0;
+    
+    size = core.rs.available(); // Get number of available bytes
     if (size) {
-        core.rs.read(data, size);
+        core.rs.read(data, size); // Read available data
         Serial.printf("%s\n", data);
     }
 
-    core.rs.write("Hello OpenIndus\r\n");
+    core.rs.write(data, size); // Echo back the received data
+
     delay(10);
 }
