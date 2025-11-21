a22ec969bd0ff59fcaf84f959fda272f28a48204
Aurélien Floutard <aurelien.floutard@openidus.com>
2025-11-21 10:30:15 +0100

    add new gif and update text

--- docs/source/_static/gif/convert_mpa_to_gif.sh
+++ docs/source/_static/gif/convert_mpa_to_gif.sh
@@ -5,4 +5,5 @@ file_name_without_extension=${file_name%%.*}
 
 ffmpeg -y -i $file_name_without_extension.mp4 -vf palettegen $file_name_without_extension.png
 ffmpeg -y -i $file_name_without_extension.mp4 -i $file_name_without_extension.png -lavfi "[0]scale=1080:trunc(ow/a/2)*2[x];[x][1:v] paletteuse" -r 5 $file_name_without_extension.gif
+# ffmpeg -ss 2 -to 42 -i input.mp4 -c copy output.mp4
 # ffmpeg -i input.gif -ss 00:00:00 -to 00:00:03 -c copy output.gif
