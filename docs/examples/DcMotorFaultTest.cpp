eb903291ab386b3d5b194183fec1120084136310
Georges de Massol <georges.massol@openindus.com>
2025-11-27 17:03:41 +0100

    fix: optimize melody playback timing; replace delays with precise timing using micros() for better tone

--- docs/examples/DcMotorFaultTest.cpp
+++ docs/examples/DcMotorFaultTest.cpp
@@ -79,15 +79,19 @@ void playMelody(bool allTestsPassed)
         melodyLen = sizeof(failureMelody) / sizeof(failureMelody[0]);
     }
 
+    unsigned long startTime = millis();
     for (int n = 0; n < melodyLen; n++) {
+        unsigned long noteStartTime = millis();
         if (melody[n].freq > 0) {
             int period_us = 1000000 / melody[n].freq;
             int cycles = (melody[n].durationMs * 1000) / period_us;
             for (int i = 0; i < cycles; i++) {
+                unsigned long cycleStartTime = micros();
                 dc.run(MOTOR_1, FORWARD, 100.0f);
-                delayMicroseconds(period_us / 2);
+                while (micros() - cycleStartTime < period_us / 2);
+                cycleStartTime = micros();
                 dc.run(MOTOR_1, REVERSE, 100.0f);
-                delayMicroseconds(period_us / 2);
+                while (micros() - cycleStartTime < period_us / 2);
             }
         } else {
             // Rest (no sound)
